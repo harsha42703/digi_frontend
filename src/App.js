3bc33b6957897092d09ec6e2dae0c46e4c212215
Harsha Vardhan <94213293+harsha42703@users.noreply.github.com>
2024-08-02 14:50:34 +0530

    sidebar added

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Sidebar from './Components/Sidebar';
+
+function App() {
+  return (
+    <div className="App">
+      <Sidebar />
+    </div>
+  );
+}
+
+export default App;
